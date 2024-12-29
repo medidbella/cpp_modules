6898b5fbca8a76d14b8b520644785ff2a13b63fe
medidbella <mohamedidbellaali@gmail.com>
2024-12-29 13:29:21 +0100

    ex01 testes

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -1,48 +1,88 @@
 #include "Fixed.hpp"
 
-const int Fixed::fractionalBitsNumber = 8;
-
-Fixed::Fixed(){
+Fixed::Fixed()
+{
 	std::cout << "Default constructor called\n";
 	value = 0;
 }
 
-Fixed::Fixed(const Fixed &dest){
+Fixed::Fixed(const Fixed &dest)
+{
 	std::cout << "Copy constructor called\n";
 	value = dest.value;
 }
 
-Fixed::Fixed(const int number){
+Fixed::Fixed(const int number)
+{
+	std::cout << "int constructor called\n";
 	value = number << fractionalBitsNumber;
 }
 
-// Fixed::Fixed(const float number){
-// 	int bits_iter;
-// 	char *ptr = (char *)&this->value;
+Fixed::Fixed(const float number)
+{
+	std::cout << "float constructor called\n";
+	value = 0;
+	int integerPart = number;
+	float fractionalPart;
 
-// 	if ((number > 0 && number < 1) || (number < 0 && number > -1))
-// 		*(ptr + 2) = 0;
-// 	else
-// 		*(ptr + 2) = roundf(number);
-// 	//fractional part
-	
-// }
+	fractionalPart = number - integerPart;
+	for (int bits = 1; bits <= fractionalBitsNumber; bits++)
+	{
+		value <<= 1;
+		fractionalPart *= 2;
+		if (fractionalPart >= 1)
+		{
+			value += 1;
+			fractionalPart -= 1;
+		}
+	}
+	value += integerPart << fractionalBitsNumber;
+}
 
-void Fixed::operator=(const Fixed &otherObject){
+void Fixed::operator =(const Fixed &otherObject)
+{
 	std::cout << "used copy assignment operator\n";
 	value = otherObject.value;
 }
 
-void Fixed::setRawBits(int newValue){
+void Fixed::setRawBits(int newValue)
+{
 	std::cout << "setRawBits member function called\n";
 	value = newValue;
 }
 
-int Fixed::getRawBits(){
+int Fixed::getRawBits() const
+{
 	std::cout << "getRawBits member function called\n";
 	return value;
 }
 
-Fixed::~Fixed(){
+float Fixed::toFloat() const
+{
+	int integerPart = value >> fractionalBitsNumber;
+	float fractionalPart = 0;
+	int divisor = 2;
+
+	for (int bits = fractionalBitsNumber - 1; bits >= 0; bits--)
+	{
+		fractionalPart += (float)1 / divisor * (value >> bits & 1);
+		divisor *= 2;
+	}
+	return (integerPart + fractionalPart);
+}
+
+int Fixed::toInt( void ) const
+{
+	return (value >> fractionalBitsNumber);
+}
+
+std::ostream& operator <<(std::ostream &out, const Fixed &number)
+{
+	out << number.toFloat();
+	return out;
+}
+
+Fixed::~Fixed()
+{
 	std::cout << "Destructor called\n";
 }
