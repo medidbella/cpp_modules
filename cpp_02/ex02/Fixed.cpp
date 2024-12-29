b65b2665391d6e53e57f69e0fe841b0842206903
medidbella <mohamedidbellaali@gmail.com>
2024-12-29 13:27:34 +0100

    finished ex01

---
+++ cpp_02/ex02/Fixed.cpp
@@ -0,0 +1,88 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called\n";
+	value = 0;
+}
+
+Fixed::Fixed(const Fixed &dest)
+{
+	std::cout << "Copy constructor called\n";
+	value = dest.value;
+}
+
+Fixed::Fixed(const int number)
+{
+	std::cout << "int constructor called\n";
+	value = number << fractionalBitsNumber;
+}
+
+Fixed::Fixed(const float number)
+{
+	std::cout << "float constructor called\n";
+	value = 0;
+	int integerPart = number;
+	float fractionalPart;
+
+	fractionalPart = number - integerPart;
+	for (int bits = 1; bits <= fractionalBitsNumber; bits++)
+	{
+		value <<= 1;
+		fractionalPart *= 2;
+		if (fractionalPart >= 1)
+		{
+			value += 1;
+			fractionalPart -= 1;
+		}
+	}
+	value += integerPart << fractionalBitsNumber;
+}
+
+void Fixed::operator =(const Fixed &otherObject)
+{
+	std::cout << "used copy assignment operator\n";
+	value = otherObject.value;
+}
+
+void Fixed::setRawBits(int newValue)
+{
+	std::cout << "setRawBits member function called\n";
+	value = newValue;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called\n";
+	return value;
+}
+
+float Fixed::toFloat() const
+{
+	int integerPart = value >> fractionalBitsNumber;
+	float fractionalPart = 0;
+	int divisor = 2;
+
+	for (int bits = fractionalBitsNumber - 1; bits >= 0; bits--)
+	{
+		fractionalPart += (float)1 / divisor * (value >> bits & 1);
+		divisor *= 2;
+	}
+	return (integerPart + fractionalPart);
+}
+
+int Fixed::toInt( void ) const
+{
+	return (value >> fractionalBitsNumber);
+}
+
+std::ostream& operator <<(std::ostream &out, const Fixed &number)
+{
+	out << number.toFloat();
+	return out;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
--- cpp_02/ex02/Fixed.hpp
+++ cpp_02/ex02/Fixed.hpp
@@ -2,21 +2,27 @@
 # define FIXED_HPP
 
 # include <cmath> 
+#include <iomanip>
 # include <iostream>
 
 class Fixed{
 	private:
 		int value;
-		static const int fractionalBitsNumber;
+		static const int fractionalBitsNumber = 8;
 	
 	public:
 		Fixed();
 		Fixed(const Fixed &dest);
 		Fixed(const int number);
-		// Fixed(const float number);
+		Fixed(const float number);
 		void operator =(const Fixed &otherObject);
 		void setRawBits(int newValue);
-		int	getRawBits();
+		int	getRawBits() const;
+		float toFloat() const;
+		int toInt(void) const;
+		~Fixed();
 };
+
+std::ostream& operator <<(std::ostream &out, const Fixed &number);
 
 # endif
