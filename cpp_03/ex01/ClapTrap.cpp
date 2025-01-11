5741dc2553643578d43a5bff15d2d32a89e02edd
medidbella <mohamedidbellaali@gmail.com>
2025-01-11 14:53:31 +0100

    added ex01

---
+++ cpp_03/ex01/ClapTrap.cpp
@@ -0,0 +1,119 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+	std::cout << "ClapTrap default constructor called\n";
+	name = "unnamed";
+	AttackDamage = 0;
+	hitPoints = 10;
+	energyPoints = 10;
+}
+
+ClapTrap::ClapTrap(std::string initName)
+{
+	std::cout << "ClapTrap constructor called\n";
+	name = initName;
+	AttackDamage = 0;
+	hitPoints = 10;
+	energyPoints = 10;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &source)
+{
+	std::cout << "ClapTrap copy assignment operator called\n";
+	name = source.name;
+	AttackDamage = source.AttackDamage;
+	hitPoints = source.hitPoints;
+	energyPoints = source.energyPoints;
+	return *this;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &dest)
+{
+	std::cout << "ClapTrap copy constructor called\n";
+	name = dest.name;
+	AttackDamage = dest.AttackDamage;
+	hitPoints = dest.hitPoints;
+	energyPoints = dest.energyPoints;
+}
+
+ClapTrap::~ClapTrap(){
+	std::cout << "ClapTrap " + name + " destructor called\n";
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (energyPoints <= 0){
+		std::cout << "ClapTrap " + name + " can't attack (insefisent energy points)\n";
+		return ;
+	}
+	else if (hitPoints <= 0){
+		std::cout << "ClapTrap " + name + " can't attack (insefisent hit points)\n";
+		return;
+	}
+	energyPoints -= 1;
+	std::cout << "ClapTrap " << name << " attacks " << target
+		<< ", causing " << AttackDamage << " points of damage!\n";
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (energyPoints <= 0){
+		std::cout << "ClapTrap " + name + " can't take damage (insefisent energy points)\n";
+		return ;
+	}
+	else if (hitPoints <= 0){
+		std::cout << "ClapTrap " + name + " can't take damage (insefisent hit points)\n";
+		return;
+	}
+	hitPoints -= amount;
+	std::cout << "ClapTrap " << name << " got attacked causing " << amount 
+		<< " decreased hit points\n";
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (energyPoints <= 0){
+		std::cout << "ClapTrap " + name + " can't be repaired (insefisent energy points)\n";
+		return ;
+	}
+	else if (hitPoints <= 0){
+		std::cout << "ClapTrap " + name + " can't be repaired (insefisent hit points)\n";
+		return;
+	}
+	energyPoints -= 1;
+	hitPoints += amount;
+	std::cout << "ClapTrap " + name + " got repaired by " << amount << "hit points\n";
+}
+
+int ClapTrap::getHitPoints(){
+	return hitPoints;
+}
+
+int ClapTrap::getEnergypoints(){
+	return energyPoints;
+}
+
+int ClapTrap::getAttackDamage(){
+	return AttackDamage;
+}
+
+std::string ClapTrap::getName(){
+	return name;
+}
+
+void ClapTrap::setName(std::string newName){
+	name = newName;
+}
+
+void ClapTrap::setAttackDamage(int newValue){
+	AttackDamage = newValue;
+}
+
+void ClapTrap::setEnergyPoints(int newValue){
+	energyPoints = newValue;
+}
+
+void ClapTrap::setHitPoints(int newValue){
+	hitPoints = newValue;
+}
---
+++ cpp_03/ex01/ScavTrap.cpp
@@ -0,0 +1,41 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap()
+{
+	std::cout << "ScavTrap default constructor called\n";
+	setName("unnamed");
+	setAttackDamage(20);
+	setEnergyPoints(50);
+	setHitPoints(100);
+}
+
+ScavTrap::ScavTrap(std::string name)
+{
+	std::cout << "ScavTrap constructor called\n";
+	setName(name);
+	setAttackDamage(20);
+	setEnergyPoints(50);
+	setHitPoints(100);
+}
+ScavTrap::~ScavTrap(){
+	std::cout << "ScavTrap " + getName() + " destructor called\n";
+}
+
+void ScavTrap::guardGate(){
+	std::cout << " ScavTrap " + getName() + "is now in Gatekeeper mode\n";
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+	if (getEnergypoints() <= 0){
+		std::cout << "ScavTrap " + getName() + " can't attack (insefisent energy points)\n";
+		return ;
+	}
+	else if (getHitPoints() <= 0){
+		std::cout << "ScavTrap " + getName() + " can't attack (insefisent hit points)\n";
+		return;
+	}
+	setEnergyPoints(getEnergypoints() - 1);
+	std::cout << "ScavTrap " << getName() << " attacks " << target
+		<< ", causing " << getAttackDamage() << " points of damage!\n";
+}
