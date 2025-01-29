d0a44c138f953f25b48cba8b576e3b44bdb03047
medidbella <mohamedidbellaali@gmail.com>
2025-01-29 10:51:25 +0100

    added cpp_07/ex01

---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+
+void swap(T elem1, T elem2)
+{
+	T tmp = elem1;
+	elem1 = elem2;
+	elem2 = tmp;
+}
+
+template <typename T>
+
+T min(T elem1, T elem2)
+{
+	return elem1 < elem2 ? elem1 : elem2;
+}
+
+template <typename T>
+
+T max(T elem1, T elem2)
+{
+	return elem1 > elem2 ? elem1 : elem2;
+}
+
+#endif
